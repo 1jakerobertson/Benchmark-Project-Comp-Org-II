cc9b38b30971175eca21fdcbd7236a615fc36a35
1jakerobertson <jacob.robertson@bison.howard.edu>
2025-04-02 21:45:03 -0400

    commit

--- benchmarkproject.cpp
+++ benchmarkproject.cpp
@@ -3,6 +3,18 @@
 #include <vector>
 #include <fstream>
 
+using namespace std;
+using namespace chrono;
+
+high_resolution_clock::time_point start_timer() {
+    return high_resolution_clock::now();
+}
+
+double stop_timer(high_resolution_clock::time_point start) {
+    auto end = high_resolution_clock::now();
+    return duration<double>(end - start).count();
+}
+
 int main() {  // Ensure this is the program entry point
   std::cout << "Benchmark started!" << std::endl;
   return 0;
